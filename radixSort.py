de38eb379cf5aedba7dd651d5807b84c90b3df54
Trin <trinity@ridley-smith.com>
2020-01-20 21:05:15 +1100

    Adding pycharm project files

---
+++ radixSort.py
@@ -0,0 +1,18 @@
+from random import *
+
+def randomArray(length):
+    newArray = []
+    for i in range(length):
+        newArray.append(randint(0, 100000))
+    return newArray
+
+def magicWow(array):
+
+    return sortedArray
+
+
+basicArray = randomArray(100)
+
+finalArray = magicWow(basicArray)
+
+print(finalArray)
